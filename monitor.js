b2799bc307a054dcaf33ec9fedce04a528930d98
Azerpas <xskylix@hotmail.fr>
2020-04-02 01:14:43 +0200

    added get availability

--- monitor.js
+++ monitor.js
@@ -33,24 +33,24 @@ async function handleGET(req,res){
 		if(mode == "loca"){
 			let carrefour = new Carrefour(codepostal);
 			const location = await carrefour.getLoca();
-			return new Promise(resolve=>{
-				if(location.lat){
-					carrefour.getStores(location.lat,location.lng,codepostal)
-						.then(stores => {
-							resolve(res.status(200).send(JSON.stringify(stores)))
-						})
-						.catch(error => {
-							console.log("Error while waiting for stores")
-							console.error(error)
-							resolve(res.status(500).send(JSON.stringify({success:false,message:"Impossible d'obtenir les magasins",error:error.message})))
-						});
-				}else{
-					resolve(res.status(500).send(JSON.stringify({success:false,message:"Impossible d'obtenir la geolocalisation: "+location.message})))
+			if(location.lat){
+				let stores = await carrefour.getStores(location.lat,location.lng,codepostal)
+				let availabilities = [];
+				for(let store of stores){
+					availabilities.push(await carrefour.getAvailability(store));
 				}
-			})
+				return new Promise(resolve=>{
+					resolve(res.status(200).send(JSON.stringify(availabilities)))
+				})
+			}else{
+				return new Promise(resolve=>{
+					resolve(res.status(500).send(JSON.stringify({success:false,message:"Impossible d'obtenir la geolocalisation: "+location.message})))
+				})
+			}
 			
 		}
 	}
+	console.log("Not getting there")
 	let zip = await monitor();
 	await res.status(200).send(JSON.stringify(zip));
 }
@@ -66,6 +66,7 @@ class Carrefour{
 		this.postcode = postcode;
 		this.woosmap = "https://api.woosmap.com/localities/autocomplete/?key=woos-26fe76aa-ff24-3255-b25b-e1bde7b7a683&components=country:fr|country:mc&input="
 		this.carrefourGeoloc = "https://www.carrefour.fr/geoloc"
+		this.availability = "https://www.carrefour.fr/api/firstslot?storeId="
 	}
 
 	async getLoca(){
@@ -141,4 +142,45 @@ class Carrefour{
 		});
 	}
 
+	async getAvailability(store){
+		return new Promise(resolve => {
+			const headers = {
+				'Host': 'www.carrefour.fr','accept': 'application/json, text/plain, */*','user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.69 Safari/537.36',
+				'x-requested-with': 'XMLHttpRequest', // needed to get redirected to their Ajax Controller
+				'sec-fetch-site': 'same-origin','sec-fetch-mode': 'cors','sec-fetch-dest': 'empty','referer': 'https://www.carrefour.fr/', 'accept-language': 'en,en-US;q=0.9,fr;q=0.8',
+			}
+			//console.log(store)
+			axios.get(this.availability+store.ref,{headers:headers})
+				.then(response => {
+					console.log(response.status)
+					if(response.data.data){
+						if(response.data.data.type){
+							resolve({"store":store,"availability":response.data.data.attributes.begDate,"error":false})
+						}else{ 
+							resolve({"store":store,"availability":null,"error":true,"message":response.data})
+						}
+					}
+					else if(JSON.stringify(response.data) === '[]'){
+						resolve({"store":store,"availability":null,"error":false})
+					}
+					else{
+						if(JSON.stringify(response.data).includes("maintenance")){
+							console.error("Carrefour est en maintenance pour le moment")
+							console.error(response.status)
+							resolve({"success":false,"message":"Carrefour est en maintenance pour le moment"})
+						}else{
+							console.error("Erreur inconnue, impossible de joindre Carrefour")
+							console.error(response.status)
+							console.error(response.data)
+							resolve({"success":false,"message":"Erreur inconnue, impossible de joindre Carrefour"})
+						}
+					}
+				})
+				.catch(error => {
+					console.error("Seems like the request to get stores went wrong")
+					console.error(error)
+					resolve({"success":false,"message":"Impossible de joindre Carrefour, impossible de trouver des magasins","error":error})
+				});
+		});
+	}
 }
